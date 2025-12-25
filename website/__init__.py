3e6898d78b523ae92417bccb324d107ee5d066e0
Andre Giske <agiske@ucsd.edu>
2025-12-24 18:24:30 -0800

    linked api's to app

--- website/__init__.py
+++ website/__init__.py
@@ -14,3 +14,22 @@ def create_app():
 	app.config['SQLALCHEMY_DATABSE_URI'] = f'sqlite:///{settings.DB_NAME}'
 
 	db.init_app(app)
+
+	from .views import views
+	from .auth import auth
+
+	app.register_blueprint(views, url_prefix='/')
+	app.register_blueprint(auth, url_prefix='/')
+
+	while app.app_context():
+		db.create_all()
+
+	login_manager = LoginManager()
+	login_manager.login_view = 'auth.login'
+	login_manager.init_app(app)
+
+	@login_manager.user_loader
+	def load_user(id):
+		return User.query.get(int(id))
+	
+	return app
--- website/main.py
+++ website/main.py
@@ -5,4 +5,4 @@ from config import settings
 app = create_app()
 
 if __name__ == '__main__':
-	app.run(debug=True, port=settings.PORT)
+	app.run(debug=True, port=int(settings.PORT))
