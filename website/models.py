101ff16295b648f3ebefa7b9d23d2a36ac1a40ff
Andre Giske <agiske@ucsd.edu>
2025-12-25 00:26:42 -0800

    variable naming

--- website/models.py
+++ website/models.py
@@ -5,39 +5,39 @@ from sqlalchemy.sql import func
 from . import db
 
 class User(db.model, UserMixin):
-	id				= db.Column(db.Integer, primary_key=True)
-	email			= db.Column(db.String(150), unique=True)
-	password		= db.Column(db.String(150))
-	first_name		= db.Column(db.String(100))
-	search_history	= db.relationship(
+	id					= db.Column(db.Integer, primary_key=True)
+	email				= db.Column(db.String(150), unique=True)
+	password			= db.Column(db.String(150))
+	first_name			= db.Column(db.String(100))
+	search_history		= db.relationship(
 		'SearchHistory',
 		backref='user',
 		lazy=True
 	)
 
 class Product(db.model):
-	id				= db.Column(db.Integer, primary_key=True)
-	product_name	= db.Column(db.String(100))
-	price			= db.Column(db.Integer)
-	country			= db.Column(db.String(2))
-	rating			= db.Column(db.Integer)
-	num_rating		= db.Column(db.Integer)
-	search_history	= db.relationship(
+	id					= db.Column(db.Integer, primary_key=True)
+	product_title		= db.Column(db.String(100))
+	product_price		= db.Column(db.Integer)
+	country				= db.Column(db.String(2))
+	poduct_star_rating	= db.Column(db.Integer)
+	product_num_ratings	= db.Column(db.Integer)
+	search_history		= db.relationship(
 		'SearchHistory',
 		backref='product',
 		lazy=True,
 	)
 
 class SearchHistory(db.model):
-	id				= db.Column(db.Integer, primary_key=True)
-	user_id			= db.Column(
+	id					= db.Column(db.Integer, primary_key=True)
+	user_id				= db.Column(
 		db.Integer,
 		db.ForgeinKey('user.id'),
 		nullable=False
 	)
-	product_id		= db.Column(
+	product_id			= db.Column(
 		db.Integer,
 		db.ForgeinKey('product.id'),
 		nullable=False
 	)
-	searched_at		= db.Column(db.DateTime(timezone=True), default=func.now())
+	searched_at			= db.Column(db.DateTime(timezone=True), default=func.now())
